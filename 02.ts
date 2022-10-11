f1221992b6b0a30c788f58977ed725bfb3243cde
masha14013 <masha14013@rambler.ru>
2022-10-11 12:11:03 +0300

    added files 02.ts and 02.test.ts - testing objects

---
+++ 02.ts
@@ -0,0 +1,28 @@
+export type GovernmentBuildingsType = {
+    type: "HOSPITAL" | "FIRE-STATION"
+    budget: number
+    stuffCount: number
+    address: AddressType
+}
+
+export type StreetType = {
+    title: string
+}
+
+export type AddressType = {
+    number?: number
+    street: StreetType
+}
+
+export type HouseType = {
+    buildedAt: number
+    repaired: boolean
+    address: AddressType
+}
+
+export type CityType = {
+    title: string
+    houses: Array<HouseType>
+    governmentBuildings: Array<GovernmentBuildingsType>
+    citizenNumber: number
+}
