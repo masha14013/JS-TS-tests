7a88ed78aa2ef31c70229bc7a8e2a74b71085a1e
masha14013 <masha14013@rambler.ru>
2022-10-11 18:31:04 +0300

    added file 04_02.ts - filter

---
+++ 04_02.ts
@@ -0,0 +1,9 @@
+import {CityType, GovernmentBuildingsType} from "../02/02_02";
+
+export function demolishHousesInTheStreet(city: CityType, street: string) {
+    city.houses = city.houses.filter( h => h.address.street.title !== street )
+}
+
+export const getBuildingsWithStaffCountGreaterThen = (buildings: Array<GovernmentBuildingsType>, number: number) => {
+    return buildings.filter(b => b.stuffCount > number)
+}
